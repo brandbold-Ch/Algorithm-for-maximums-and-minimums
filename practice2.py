01b3c670fb3668a80c4cd4e02fd1d95301849be3
Brandon Jared Molina Vazquez <118649094+brandbold-Ch@users.noreply.github.com>
2024-07-23 00:27:02 -0600

    Update practice2.py

--- practice2.py
+++ practice2.py
@@ -6,12 +6,6 @@ function = lambda x: x**2 - 10*x
 
 list_range_x = list(range(-10, 10, 1))
 list_range_y = [function(x) for x in range(-10, 10, 1)]
-points = list(zip(list_range_x, list_range_y))
 
-#print(list_range_x[int((len(list_range_x) - 1) / 2)])
-#print(list_range_y[int((len(list_range_y) - 1) / 2)])
-
-print(list(zip(list_range_x, list_range_y)))
-
-critical_point = [function(x[0]) == x[1] and function(x[1]) != 0 for x in points]
-print(critical_point)
+print(list_range_x[int((len(list_range_x) - 1) / 2)])
+print(list_range_y[int((len(list_range_y) - 1) / 2)])
